85388075aa4788afa3ec262e7f7de332ccc0c3d8
Nick Farina <farina.nick@gmail.com>
2021-09-15 21:03:31 -0400

    adds order table sorting functionality

--- app/javascript/orders/OrderTable.js
+++ app/javascript/orders/OrderTable.js
@@ -1,27 +1,63 @@
 // @ts-check
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-export const OrderTable = ({ orders }) => (
-  <table className="table orders-table">
-    <thead>
-      <tr>
-        <th>Order #</th>
-        <th>Ordered at</th>
-        <th>Pick up at</th>
-        <th>Customer Name</th>
-        <th>Item</th>
-        <th>Qty</th>
-        <th>Status</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {orders.map((order) => (
-        <OrderRow order={order} />
-      ))}
-    </tbody>
-  </table>
-)
+export const OrderTable = ({ orders }) => {
+  const [sortProperty, setSortProperty] = useState({
+    attribute: "",
+    ascending: true,
+  })
+
+  const sortBy = (attribute) => {
+    if (sortProperty.attribute === attribute) {
+      setSortProperty({
+        attribute,
+        ascending: !sortProperty.ascending,
+      })
+    } else {
+      setSortProperty({ attribute, ascending: true })
+    }
+  }
+
+  const orderSortFn = (a, b) => {
+    if (!sortProperty.attribute) {
+      return 1
+    }
+
+    if (a[sortProperty.attribute] > b[sortProperty.attribute]) {
+      return sortProperty.ascending ? 1 : -1
+    } else {
+      return sortProperty.ascending ? -1 : 1
+    }
+  }
+
+  return (
+    <table className="table orders-table">
+      <thead>
+        <tr>
+          <th className="sortable" onClick={() => sortBy("id")}>
+            Order #
+          </th>
+          <th>Ordered at</th>
+          <th className="sortable" onClick={() => sortBy("pick_up_at")}>
+            Pick up at
+          </th>
+          <th className="sortable" onClick={() => sortBy("customer_name")}>
+            Customer Name
+          </th>
+          <th>Item</th>
+          <th>Qty</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orders.sort(orderSortFn).map((order) => (
+          <OrderRow order={order} />
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 const OrderRow = ({ order }) => {
   return (
