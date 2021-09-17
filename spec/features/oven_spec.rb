947323f41a23c3a6ba9f9f356bcc56f3aee3e72d
Nick Farina <farina.nick@gmail.com>
2021-09-16 23:05:44 -0400

    add some specs for cookies ready/not status

--- spec/features/oven_spec.rb
+++ spec/features/oven_spec.rb
@@ -18,4 +18,48 @@ feature 'Oven' do
     expect(current_path).to eq(oven_path(oven))
     expect(page).to have_content oven.name
   end
+
+  scenario 'Oven is empty' do
+    user = create_and_signin
+    oven = user.ovens.first
+
+    visit root_path
+    click_link "View your ovens"
+    click_link oven.name
+
+    expect(current_path).to eq(oven_path(oven))
+    expect(page).to have_content oven.name
+    expect(page).to have_content 'Curently in oven: Nothing'
+    expect(page).to_not have_content "- and they're ready!"
+  end
+
+  scenario 'Oven is full, cookies not finished' do
+    user = create_and_signin
+    oven = user.ovens.first
+    Cookie.create!(fillings: 'PB&J', storage: oven)
+
+    visit root_path
+    click_link "View your ovens"
+    click_link oven.name
+
+    expect(current_path).to eq(oven_path(oven))
+    expect(page).to have_content oven.name
+    expect(page).to have_content 'Curently in oven: 1 PB&J cookie(s)'
+    expect(page).to have_css('.retrieve-section', visible: 'hidden', text: "- and they're ready!")
+  end
+
+  scenario 'Oven is full, cookies are finished' do
+    user = create_and_signin
+    oven = user.ovens.first
+    Cookie.create!(fillings: 'PB&J', storage: oven, ready: true)
+
+    visit root_path
+    click_link "View your ovens"
+    click_link oven.name
+
+    expect(current_path).to eq(oven_path(oven))
+    expect(page).to have_content oven.name
+    expect(page).to have_content 'Curently in oven: 1 PB&J cookie(s)'
+    expect(page).to have_css('.retrieve-section', visible: 'visible', text: "- and they're ready!")
+  end
 end
